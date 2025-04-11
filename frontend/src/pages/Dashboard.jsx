bdc8bb28a412c1903eddf902f2128d429154b5d1
Saurabh8840 <saurabhofficial8840@gmail.com>
2025-04-11 23:44:54 +0530

    Intitial commit

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import { Appbar } from "../components/Appbar"
+
+export const Dashboard = () => {
+    return <div>
+        <Appbar />
+        <div className="m-8">
+           
+            <h1>Hello my frnd i m dash board</h1>
+        </div>
+    </div>
+}
